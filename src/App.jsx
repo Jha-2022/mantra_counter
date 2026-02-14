5842c2e916306963ebfc22c667d9a227daea5dc4
Jha-2022 <rishijha0910@gmail.com>
2026-02-14 23:00:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    {/* <div id = "shivimg">
+      <img src="./public/shiv1.jfif" alt="" />
+    </div> */}
+    <div>
+      <img src="./public/shiv1.jfif" alt  ="mantra" id = "photo"/>
+    </div>
+    <div className="card">
+       <button onClick={() => setCount((count) => count + 1)}>
+        <h4>ॐ त्र्यम्बकं यजामहे सुगन्धिं पुष्टिवर्धनम् ।</h4>
+        <h4>उर्वारुकमिव बन्धनान् मृत्योर्मुक्षीय मामृतात् ॥ </h4> 
+        {count}
+       </button>
+    </div>     
+    </>
+  )
+}
+
+export default App
